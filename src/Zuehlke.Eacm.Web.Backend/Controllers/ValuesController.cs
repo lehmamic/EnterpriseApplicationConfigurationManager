5f2b5a5775d33927fa16b2f36c4395bcb2d0f5a1
Michael Lehmann <leh@zuehlke.com>
2016-05-30 15:30:24 +0200

    chore(sln): fix solution and namespace naming.

--- src/Zuehlke.Eacm.Web.Backend/Controllers/ValuesController.cs
+++ src/Zuehlke.Eacm.Web.Backend/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Zuehke.Eacm.Web.Backend.Controllers
+namespace Zuehlke.Eacm.Web.Backend.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
