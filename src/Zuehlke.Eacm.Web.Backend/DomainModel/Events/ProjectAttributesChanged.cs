c700eb725ffd893a5bfb938efecb49b416c3c937
Michael Lehmann <leh@zuehlke.com>
2016-06-07 18:44:50 +0200

    feat(configurationmodel): Implement the first event (project attribute changed).

---
+++ src/Zuehlke.Eacm.Web.Backend/DomainModel/Events/ProjectAttributesChanged.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zuehlke.Eacm.Web.Backend.DomainModel.Events
+{
+    public class ProjectAttributesChanged : IEvent
+    {
+        #region Implementation of IEvent
+        public Guid Id { get; set; }
+
+        public Guid SourceId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public Guid CorrelationId { get; set; }
+        #endregion
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
