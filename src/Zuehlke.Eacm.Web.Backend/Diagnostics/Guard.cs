9e8d19a889542e36b1cc291f5edbcb2f0bdb903c
Michael Lehmann <leh@zuehlke.com>
2016-07-01 16:18:05 +0200

    feat(guard): implement a guard for checking list items on null or empty.

--- src/Zuehlke.Eacm.Web.Backend/Diagnostics/Guard.cs
+++ src/Zuehlke.Eacm.Web.Backend/Diagnostics/Guard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Zuehlke.Eacm.Web.Backend.Diagnostics
 {
@@ -25,21 +26,34 @@ namespace Zuehlke.Eacm.Web.Backend.Diagnostics
         }
 
 
-        ////public static T ItemsNotNull<T>(this T source, string parameterName)
-        ////    where T : IEnumerable<object>
-        ////{
-        ////    if (string.IsNullOrWhiteSpace(source))
-        ////    {
-        ////        throw new ArgumentException($"The parameter {parameterName} should not be null or empty.", parameterName);
-        ////    }
+        public static IEnumerable<T> ItemsNotNull<T>(this IEnumerable<T> source, string parameterName)
+        {
+            source.ArgumentNotNull(parameterName);
 
-        ////    return source;
-        ////}
+            if (source.Any(i => i == null))
+            {
+                throw new ArgumentException($"The parameter {parameterName} should not contain items which are null.", parameterName);
+            }
+
+            return source;
+        }
+
+        public static IEnumerable<string> ItemsNotNullOrEmpty(this IEnumerable<string> source, string parameterName)
+        {
+            source.ArgumentNotNull(parameterName);
+
+            if (source.Any(string.IsNullOrWhiteSpace))
+            {
+                throw new ArgumentException($"The parameter {parameterName} should not contain items which are null or empty.", parameterName);
+            }
+
+            return source;
+        }
 
 
         public static T InvalidCondition<T>(this T source, Predicate<T> expectedCondition, string message, string parameterName)
         {
-            source.ArgumentNotNull(nameof(source));
+            source.ArgumentNotNull(parameterName);
 
             if (!expectedCondition(source))
             {
