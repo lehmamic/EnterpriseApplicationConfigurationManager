3f8e3cdcef022c04d6d34208a32b816b6d2b75ab
Michael Lehmann <lehmamic@bluewin.ch>
2016-08-18 21:02:27 +0200

    refactor(configurationproject): Implement tests for the EventAggregatorExtensions class.

--- test/Zuehlke.Eacm.Web.Backend.Tests/CQRS/EventAggregatorExtensionsFixture.cs
+++ test/Zuehlke.Eacm.Web.Backend.Tests/CQRS/EventAggregatorExtensionsFixture.cs
@@ -1,19 +1,49 @@
 using System;
 using Xunit;
 using Zuehlke.Eacm.Web.Backend.CQRS;
+using Zuehlke.Eacm.Web.Backend.Utils.PubSubEvents;
 
 namespace Zuehlke.Eacm.Web.Backend.Tests.DomainModel
 {
     public class EventAggregatorExtensionsFixture
     {
         [Fact]
-        public void PublishEvent_()
+        public void PublishEvent_EventAggregatorIsNull_ThrowsException()
         {
-            // arrange 
+            // arrange
+            IEventAggregator eventAggegator = null;
+            IEvent e = new TestEvent();
 
             // act
+            Assert.ThrowsAny<ArgumentNullException>(() => CQRS.EventAggregatorExtensions.PublishEvent(eventAggegator, e));
+        }
 
-            // assert
+        [Fact]
+        public void PublishEvent_EventIsNull_ThrowsException()
+        {
+            // arrange
+            IEventAggregator eventAggegator = new EventAggregator();
+            IEvent e = null;
+
+            // act
+            Assert.ThrowsAny<ArgumentNullException>(() => CQRS.EventAggregatorExtensions.PublishEvent(eventAggegator, e));
+        }
+
+        [Fact]
+        public void PublishEvent_WithEventSubscription_HandlerGetsCalleds()
+        {
+            // arrange
+            IEventAggregator eventAggegator = new EventAggregator();
+            IEvent e = new TestEvent();
+
+            bool executed = false;
+
+            eventAggegator.Subscribe<TestEvent>(ev => executed = true);
+
+            // act
+            CQRS.EventAggregatorExtensions.PublishEvent(eventAggegator, e);
+
+            Assert.True(executed);
         }
 
         private class TestEvent : IEvent
