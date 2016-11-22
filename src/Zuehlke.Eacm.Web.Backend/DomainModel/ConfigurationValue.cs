96ea2ea8f94f5e0f289c6c8ce646177683d6376a
Michael Lehmann <lehmamic@bluewin.ch>
2016-11-22 21:44:09 +0100

    feat(ConfigurationModel): Implement and add tests for delete and add property definitions effect on the configuration entries.

--- src/Zuehlke.Eacm.Web.Backend/DomainModel/ConfigurationValue.cs
+++ src/Zuehlke.Eacm.Web.Backend/DomainModel/ConfigurationValue.cs
@@ -1,19 +1,36 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Zuehlke.Eacm.Web.Backend.Diagnostics;
+using Zuehlke.Eacm.Web.Backend.DomainModel.Events;
 using Zuehlke.Eacm.Web.Backend.Utils.PubSubEvents;
 
 namespace Zuehlke.Eacm.Web.Backend.DomainModel
 {
     public class ConfigurationValue : ModelNode
     {
+        private string currentPropertyType;
+
         public ConfigurationValue(IEventAggregator eventAggregator, PropertyDefinition property, object value)
             : base(eventAggregator, property.ArgumentNotNull(nameof(property)).Id)
         {
             this.Property = property;
             this.Value = value;
+
+            this.currentPropertyType = this.Property.PropertyType;
+
+            this.EventAggregator.Subscribe<PropertyDefinitionModified>(this.OnPropertyDefinitionModified, e => e.PropertyId == this.Property.Id);
         }
 
         public PropertyDefinition Property { get; }
 
         public object Value { get; private set; }
+
+        private void OnPropertyDefinitionModified(PropertyDefinitionModified e)
+        {
+            if (this.currentPropertyType != e.PropertyType)
+            {
+                this.currentPropertyType = this.Property.PropertyType;
+                this.Value = null;
+            }
+        }
     }
 }
