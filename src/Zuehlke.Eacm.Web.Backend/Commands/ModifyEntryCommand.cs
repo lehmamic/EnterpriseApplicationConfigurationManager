812218416daedc83abcbc98e87f3cda592a36e2e
Michael Lehmann <lehmamic@bluewin.ch>
2016-12-06 15:03:50 +0100

    feat(EventSourcing): Implement the Create, Modify and Delete Entry commands

---
+++ src/Zuehlke.Eacm.Web.Backend/Commands/ModifyEntryCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using CQRSlite.Commands;
+
+namespace Zuehlke.Eacm.Web.Backend.Commands
+{
+    public class ModifyEntryCommand : ICommand
+    {
+        public Guid Id { get; set; }
+
+        public Guid EntryId { get; set; }
+
+        public IEnumerable<object> Values { get; set; }
+
+        public int ExpectedVersion { get; set; }
+    }
+}
