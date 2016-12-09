c2be2726237e9e2d17ad54cd716c1b141021ff3f
Michael Lehmann <leh@zuehlke.com>
2016-12-09 10:32:17 +0100

    feat(api): Initialize an end to end test project

---
+++ test/Zuehlke.Eacm.Web.Backend.EndToEndTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zuehlke.Eacm.Web.Backend.EndToEndTests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
