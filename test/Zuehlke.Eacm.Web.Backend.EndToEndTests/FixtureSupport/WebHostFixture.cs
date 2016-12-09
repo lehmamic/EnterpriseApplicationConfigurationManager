e08697eea6807cce1e3741b40b4240b27adcefb8
Michael Lehmann <leh@zuehlke.com>
2016-12-09 11:52:36 +0100

    feat(api): Initialize end to end tests (not working yet)

---
+++ test/Zuehlke.Eacm.Web.Backend.EndToEndTests/FixtureSupport/WebHostFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Zuehlke.Eacm.Web.Backend.DataAccess;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Zuehlke.Eacm.Web.Backend.EndToEndTests.FixtureSupport
+{
+    public class WebHostFixture<TStartup> : IDisposable where TStartup : class
+    {
+        private readonly TestServer server;
+
+        public WebHostFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<TStartup>()
+                .ConfigureServices(services => services.AddDbContext<EacmDbContext>(opt => opt.UseInMemoryDatabase()));
+
+            this.server = new TestServer(builder);
+
+            this.Client = this.server.CreateClient();
+            this.Client.BaseAddress = new Uri("http://localhost:5000");
+        }
+
+        public HttpClient Client { get; }
+
+        public void Dispose()
+        {
+            this.Client.Dispose();
+            this.server.Dispose();
+        }
+    }
+}
