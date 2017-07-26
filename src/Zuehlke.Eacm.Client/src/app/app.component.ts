c9ff3fd7b882d34ceb1d1caef90366b5203260ee
Michael Lehmann <lehmamic@bluewin.ch>
2017-07-26 22:56:43 +0200

    initialize ng rx

--- src/Zuehlke.Eacm.Client/src/app/app.component.ts
+++ src/Zuehlke.Eacm.Client/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Store } from '@ngrx/store';
+
+import { RootState } from './app.state';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private store: Store<RootState>, private titleService: Title) {
+    store.select(s => s.app.pageTitle)
+         .subscribe((title) => titleService.setTitle(title));
+  }
 }
--- src/Zuehlke.Eacm.Client/src/app/app.module.ts
+++ src/Zuehlke.Eacm.Client/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
-import 'hammerjs';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { RouterModule } from '@angular/router';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { environment } from '../environments/environment'
+import { reducers } from './app.reducer';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -19,7 +27,12 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    StoreModule.forRoot(reducers),
+    //RouterModule.forRoot(routes, { useHash: false }),
+    //StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
